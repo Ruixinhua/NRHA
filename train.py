31f905f53218f65c9ef913c77a05fe76a1140c96
Dairui Liu <rxhjxja@icloud.com>
2021-01-18 15:44:12 +0000

    update training process

--- train.py
+++ train.py
@@ -1,50 +1,34 @@
-import torch
-
-from callbacks import TestCallback, ValidationCallback
-from configuration import get_path, get_params
-import argparse
+from callbacks import ValidationCallback
+from configuration import get_path, get_params, get_argument, get_model_class
 import pytorch_lightning as pl
 import pytorch_lightning.loggers as pl_loggers
 
 from data_loader.training_dataset import TrainingDataset
 from torch.utils.data.dataloader import DataLoader
-
-from models.base import BaseModel
-from models.nrha_title import NRHATitle
 from utils.helpers import get_converter
 
-
-parse = argparse.ArgumentParser(description="Training process")
-parse.add_argument("--log", "-l", dest="log", metavar="FILE", help="log file", default="test")
-parse.add_argument("--configure", "-c", dest="config", metavar="FILE", help="yaml file", default=r"config/nrha.yaml")
-parse.add_argument("--gup_num", "-g", dest="gpus", metavar="INT", default=1, help="The number of gpu")
-parse.add_argument("--model_class", "-m", dest="model_class", metavar="TEXT", default="nrha")
-parse.add_argument("--mind_type", "-t", dest="mind_type", metavar="TEXT", default="large")
-parse.add_argument("--resume", "-r", dest="resume", metavar="INT", help="whether resume or not", default=0)
-args = parse.parse_args()
+args = get_argument()
+hparams = get_params(args.config, head_num=int(args.head_num), head_dim=int(args.head_dim),
+                     batch_size=int(args.batch_size))
+accelerator = "ddp" if int(args.gpus) > 1 else None
+print(args)
 train_news_file, train_behaviors_file = get_path("train", mind_type=args.mind_type)
 valid_news_file, valid_behaviors_file = get_path("valid", mind_type=args.mind_type)
 test_news_file, test_behaviors_file = get_path("test", mind_type=args.mind_type)
-config = {"yaml_name": args.config, "log": args.log, "gpus": int(args.gpus),
-          "model_class": args.model_class, "mind_type": args.mind_type}
-accelerator = "ddp" if config["gpus"] > 1 else None
-hparams = get_params(args.config)
+
 converter = get_converter(hparams.embedding, word_dict_file=hparams.word_dict_file)
 train_dataset = TrainingDataset(train_news_file, train_behaviors_file, hparams, converter, npratio=hparams.npratio)
 train_dataloader = DataLoader(train_dataset, hparams.batch_size)
 # TODO: modify value
-interval, epochs = len(train_dataloader) // 8, 5
-saved_dir = f"{config['mind_type']}/{config['model_class']}"
+interval, epochs = len(train_dataloader) // 3, 8
+saved_dir = f"{args.mind_type}/{args.model_class}"
 ckpt_dir = f"saved/checkpoint/{saved_dir}/{args.log}"
 pred_dir = f"saved/prediction/{saved_dir}/{args.log}"
 valid_callback = ValidationCallback(valid_news_file, valid_behaviors_file, hparams, converter, ckpt_dir, interval)
 # TODO: modify name
 tb_logger = pl_loggers.TensorBoardLogger(f"saved/logs/{saved_dir}", name=args.log)
-if config["model_class"] == "nrha":
-    model_class = NRHATitle
-else:
-    model_class = BaseModel
+model_class = get_model_class(args.model_class)
 # most basic trainer, uses good defaults (auto-tensorboard, checkpoints, logs, and more)
-trainer = pl.Trainer(gpus=config["gpus"], accelerator=accelerator, max_epochs=epochs, deterministic=True,
+trainer = pl.Trainer(gpus=int(args.gpus), accelerator=accelerator, max_epochs=epochs, deterministic=True,
                      logger=tb_logger, callbacks=[valid_callback])
 trainer.fit(model_class(hparams), train_dataloader)
